27660e9015388c12b88bdd4bae9e8745cda34179
Abdul R. Wahab <arwahab@users.noreply.github.com>
2018-06-17 17:01:07 -0400

    Add files via upload

---
+++ src/HashCodeEqualsOverridenDemo.java
@@ -0,0 +1,78 @@
+
+// Java program to illustrate
+// overriding of equals and
+// hashcode methods
+import java.io.*;
+import java.util.*;
+
+class Geek {
+
+	String name;
+	int id;
+
+	Geek(String name, int id) {
+
+		this.name = name;
+		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+
+		// if both the object references are
+		// referring to the same object.
+		if (this == obj)
+			return true;
+
+		// it checks if the argument is of the
+		// type Geek by comparing the classes
+		// of the passed argument and this object.
+		// if(!(obj instanceof Geek)) return false; ---> avoid.
+		if (obj == null || obj.getClass() != this.getClass())
+			return false;
+
+		// type casting of the argument.
+		Geek geek = (Geek) obj;
+
+		// comparing the state of argument with
+		// the state of 'this' Object.
+		return (geek.name == this.name && geek.id == this.id);
+	}
+
+	@Override
+	public int hashCode() {
+
+		// We are returning the Geek_id
+		// as a hashcode value.
+		// we can also return some
+		// other calculated value or may
+		// be memory address of the
+		// Object on which it is invoked.
+		// it depends on how you implement
+		// hashCode() method.
+		return this.id;
+	}
+
+}
+
+// Driver code
+class HashCodeEqualsOverridenDemo {
+	public static void main(String[] args) {
+
+		// creating Objects with
+		// same state
+		Geek g1 = new Geek("tony", 1);
+		Geek g2 = new Geek("tony", 1);
+		Geek g3 = new Geek("tonyy", 1);
+
+		Map<Geek, String> map = new HashMap<Geek, String>();
+		map.put(g1, "IT");
+		map.put(g2, "CSE");
+		map.put(g3, "ITT");
+
+		for (Geek geek : map.keySet()) {
+			System.out.println(map.get(geek).toString());
+		}
+
+	}
+}
